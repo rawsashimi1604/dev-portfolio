11d07f2f3deb2aa0bad79e44faf69f73da5c2af4
Gavin Loo <looweiren@gmail.com>
2022-09-11 00:18:00 +0800

    fixed &apos

--- components/modules/home/About.js
+++ components/modules/home/About.js
@@ -5,15 +5,15 @@ import MoreLink from '@/components/common/MoreLink'
 
 function About() {
   return (
-    <section className="mb-6 animate__animated animate__fadeIn animate__delay-1s text-lg ">
+    <section className="mb-6 animate__animated animate__fadeIn animate__delay-1s text-lg">
       <HeadlineText text="ABOUT" />
       
       <article className='border-l-4 pl-3 lg:pl-4 border-gray-600 mb-4'>
         <div className='mt-4'>
-          Hi! I'm Gavin, a second-year student in Singapore Institute of Technology currently pursuing a degree in Software Engineering. My specialties include quickly learning new skills and programming languages, problem-solving, and writing clean code.
+          Hi! I &apos; m Gavin, a second-year student in Singapore Institute of Technology currently pursuing a degree in Software Engineering. My specialties include quickly learning new skills and programming languages, problem-solving, and writing clean code.
         </div>
 
-        <div className='mt-4 '>
+        <div className="mt-4">
           In my free time, you might find me: Jamming along to music, following Leicester City Football Club, scrolling through Reddit, binge eating Japanese food, or playing video games with my friends!
         </div>
       </article>
