f1927fb832dae9e015df04f3dba52997d289dd18
Gavin Loo <looweiren@gmail.com>
2023-05-18 01:03:30 +0800

    fix architecture of website

--- components/blogs/CSSDecoded.js
+++ components/blogs/CSSDecoded.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-import blogs from "data/blogs";
+import Image from "next/image";
 
 import BlogContainer from "../modules/blog/BlogContainer";
 
-const BLOG_DATA = blogs["css-decoded"];
-
-function CSSDecoded() {
+function CSSDecoded({ data }) {
   return (
-    <BlogContainer title={BLOG_DATA.title} subtitle={BLOG_DATA.subtitle}>
-      CSS Decoded!
+    <BlogContainer title={data.title} subtitle={data.subtitle}>
+      <Image
+        src={data.image}
+        layout="responsive"
+        width="100%"
+        height="62.5%" // 16:10 aspect ratio
+        objectFit="cover"
+      />
     </BlogContainer>
   );
 }
---
+++ pages/blog/css-decoded.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import blogs from 'data/blogs'
+import CSSDecoded from '@/components/blogs/CSSDecoded'
+
+
+function cssDecoded() {
+  return (
+    <CSSDecoded data={blogs['css-decoded']}/>
+  )
+}
+
+export default cssDecoded
