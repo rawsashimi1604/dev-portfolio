e841b62bc0ad956fa7d24a026e6b7a0c115d05dc
Gavin Loo <looweiren@gmail.com>
2023-06-01 00:44:42 +0800

    update codeblock

--- components/common/CodeBlock.js
+++ components/common/CodeBlock.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function CodeBlock({ code }) {
-  return (
-    <div className="bg-gray-100 p-4 rounded-md">
-      <code className="text-sm font-mono text-gray-800 whitespace-pre">{code}</code>
-    </div>
-  );
+  return <code className="bg-gray-700 px-2.5 py-[1px] text-white rounded-lg">{code}</code>;
 }
 
 export default CodeBlock;
