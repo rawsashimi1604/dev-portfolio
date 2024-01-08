5203163a56dd097f412ac4d0d8edf0597a2e471e
rawsashimi1604 <looweiren@gmail.com>
2024-01-09 01:06:49 +0800

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import "animate.css";
 import Navbar from "@/components/layouts/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Analytics />
       <div className="text-black bg-white w-screen">
         <div className="container mx-auto px-1 lg:px-3 max-w-[1200px]">
           <Navbar />
